640535883f3b0b1a57279b109a57004b912f5f03
Yasuo Higa <higayasuo@gmail.com>
2025-06-16 08:26:07 +0900

    Add test to `x25519GetSharedSecret` for matching Web Crypto API deriveBits result, including key conversion to JWK format.

--- src/primitives/x25519/__tests__/x25519GetSharedSecret.spec.ts
+++ src/primitives/x25519/__tests__/x25519GetSharedSecret.spec.ts
@@ -3,6 +3,8 @@ import { x25519 } from '../../../x25519/x25519';
 import { randomBytes as cryptoRandomBytes } from 'crypto';
 import type { RandomBytes } from '../../../types';
 import { x25519GetSharedSecret } from '../x25519GetSharedSecret';
+import { x25519ToJwkPrivateKey } from '../x25519ToJwkPrivateKey';
+import { x25519ToJwkPublicKey } from '../x25519ToJwkPublicKey';
 
 const randomBytes: RandomBytes = (bytesLength?: number): Uint8Array => {
   return new Uint8Array(cryptoRandomBytes(bytesLength ?? 32));
@@ -28,6 +30,60 @@ describe('x25519GetSharedSecret', () => {
     );
 
     expect(aliceSharedSecret).toEqual(bobSharedSecret);
+  });
+
+  it('should match Web Crypto API deriveBits result', async () => {
+    const curve = x25519(randomBytes);
+    const alicePrivateKey = curve.utils.randomPrivateKey();
+    const bobPrivateKey = curve.utils.randomPrivateKey();
+    const bobPublicKey = curve.getPublicKey(bobPrivateKey);
+
+    // Convert keys to JWK format
+    const aliceJwkPrivateKey = x25519ToJwkPrivateKey(curve, alicePrivateKey);
+    const bobJwkPublicKey = x25519ToJwkPublicKey(curve, bobPublicKey);
+
+    // Import keys using Web Crypto API
+    const aliceCryptoKey = await crypto.subtle.importKey(
+      'jwk',
+      aliceJwkPrivateKey,
+      {
+        name: 'X25519',
+      },
+      false,
+      ['deriveBits'],
+    );
+
+    const bobCryptoKey = await crypto.subtle.importKey(
+      'jwk',
+      bobJwkPublicKey,
+      {
+        name: 'X25519',
+      },
+      false,
+      [],
+    );
+
+    // Derive shared secret using Web Crypto API
+    const webCryptoSharedSecret = new Uint8Array(
+      await crypto.subtle.deriveBits(
+        {
+          name: 'X25519',
+          public: bobCryptoKey,
+        },
+        aliceCryptoKey,
+        256,
+      ),
+    );
+
+    // Derive shared secret using x25519GetSharedSecret
+    const x25519SharedSecret = x25519GetSharedSecret(
+      curve,
+      alicePrivateKey,
+      bobPublicKey,
+    );
+
+    // Compare results
+    expect(x25519SharedSecret).toEqual(webCryptoSharedSecret);
   });
 
   it('should throw an error for invalid private key', () => {
